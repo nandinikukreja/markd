33f77c1ac1483d46d77b41b431c05d8bcbd3950a
harshpreet931 <harshpreet0402@gmail.com>
2024-11-24 20:50:12 +0530

    Set up backend server with Express and add API proxy to Vite configuration

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 8081;
+
+app.use(express.json());
+app.use("*", (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
+
+app.get("/api", (req, res) => {
+    res.send("Hello World from the Server");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">{message}</h1>
+    </>
+  );
+};
 
 export default App;
