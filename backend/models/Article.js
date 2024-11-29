df255b3d383a566dd9c7548dfc636e7477d02b3c
harshpreet931 <harshpreet0402@gmail.com>
2024-11-29 20:16:04 +0530

    Add Article model with schema definition for title, content, author, tags, and createdAt

---
+++ backend/models/Article.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    tags: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+export default Article;
