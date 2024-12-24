81146577e8a7a9c43e2fcb09b85e320fc0d60bac
harshpreet931 <harshpreet0402@gmail.com>
2024-12-24 16:04:07 +0530

    Improved Comments to auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,26 @@
 import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
+    // Get the Authorization header from the request & extract the token
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(" ")[1];
 
+    // If token is not present, return 401 Unauthorized respone
     if(!token)
         return res.status(401).json({ message: "Access Denied" });
 
-    try {
+    try 
+    {
+        // Verify the token using the secret key
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+        // Attach the decoded user information to the request object
         req.user = decoded;
+        // Proceed to the next middleware
         next();
     }
-    catch(err) {
-        console.log(err);
+    catch(err) 
+    {
+        // 403 Forbidden response if the token is invalid
         res.status(403).json({ message: "Invalid Token" });
     }
 }
