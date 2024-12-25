4fd4a60e2599d5fbaa5eeae2ac0da545d1ae33b2
harshpreet931 <harshpreet0402@gmail.com>
2024-12-25 11:44:32 +0530

    Now all the backend is readable with proper comments

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,14 @@ import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import articleRoutes from './routes/articles.js';
 
+// Load the environment variables from .env file.
 dotenv.config();
 
+// Initialize the express app!
 const app = express();
 const port = process.env.PORT;
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.DB_CONNECTION_STRING)
   .then(() => {
@@ -19,7 +22,10 @@ mongoose
     console.log("Error connecting to the server:", err);
   });
 
+// Middlewares to parse JSON data and set headers
 app.use(express.json());
+
+// Middleware to handle CORS currently allowing all origins cause there is a bug.
 app.use("*", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", `*`);
   res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
@@ -35,7 +41,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/articles', articleRoutes);
 
-
+// We listen on the port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
